1847813765c97579946c1b3cdf32978806065416
chai <chaithra.amin98@gmail.com>
2022-12-05 11:53:54 +0530

    audio play and pause using javascript and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+declare var WaveSurfer: { create: (arg0: { container: string; waveColor: string; progressColor: string; barWidth: any, responsive: boolean, height: any, barRadius: any }) => any; };
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'song-soundwave-pattern_pause_play';
+
+  ngOnInit() {
+    var wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: '#ddd',
+      progressColor: '#ff006c',
+      barWidth: 4,
+      responsive: true,
+      height: 90,
+      barRadius: 4
+
+    });
+
+    wavesurfer.load('/assets/media/Good-Day-Wake-Up-NEFFEX.mp3');
+
+    var playBtn: any = document.getElementById("playBtn");
+    playBtn.onclick = function () {
+      debugger
+      wavesurfer.playPause();
+      if (playBtn.src.includes("/assets/media/play.png")) {
+        playBtn.src = "/assets/media/pause.png";
+      }
+      else {
+        playBtn.src = "/assets/media/play.png";
+      }
+
+    }
+    wavesurfer.on('finish', function () {
+      playBtn.src = "/assets/media/play.png";
+      wavesurfer.stop();
+    })
+  }
 }
